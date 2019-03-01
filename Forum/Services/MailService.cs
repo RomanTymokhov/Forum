f7a139180196312a6b789bc1bb362099d908497a
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-03-01 08:25:40 +0200

    Create MailService.cs

---
+++ Forum/Services/MailService.cs
@@ -0,0 +1,34 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Services
+{
+    public class MailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация форума", "admin@forum.ua"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.@meta.ua", 25, false);
+                await client.AuthenticateAsync("tohoff82@meta.ua", "Meta20!9");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
