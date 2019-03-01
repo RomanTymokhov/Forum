5b901b6f15af11fb21de7f951242267564643a9e
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-03-01 07:47:00 +0200

    Create AppContext.cs

---
+++ Forum/Persistance/Contexts/AppContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Persistance.Contexts
+{
+    public class AppContext : IdentityDbContext<IdentityUser>
+    {
+        public AppContext(DbContextOptions<AppContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
