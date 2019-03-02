513f3bb6a03febf0e8b93c90b3bd60c7a98b6ea5
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-03-02 08:49:52 +0200

    Create ForumController.cs

---
+++ Forum/Controllers/ForumController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Forum.Domain.Repositories;
+using Forum.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Controllers
+{
+    public class ForumController : Controller
+    {
+        public readonly IUpdateRepo updateRepo;
+
+        public ForumController(IUpdateRepo updateRepo)
+        {
+            this.updateRepo = updateRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Themes()
+        {
+            var tvm = new ThemeViewModel { Themes = await updateRepo.ToListAsync() };
+
+            return View(new ThemeViewModel { Themes = await updateRepo.ToListAsync() });
+        }
+    }
+}
